d388fa577bbddd3a347509c8753bd48d81f5be73
ndeabreu2002 <ndeabreu2002@gmail.com>
2023-10-12 11:52:12 -0400

    ejercicio 2

---
+++ src/DistanciaTrueno.java
@@ -0,0 +1,12 @@
+//Nelson De Abreu
+//Escribir un programa que calcule la distancia en metros
+
+public class DistanciaTrueno {
+
+    public static void main(String[] args) {
+        double velcidadsonido=340.0;
+        double intervalo=7.2;
+        double distancia=velcidadsonido*intervalo;
+        System.out.println("La distancia del trueno es "+distancia+" metros");
+    }
+}
